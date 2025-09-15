5a83b1b771c51f00b4a40008e490fd2767a1cd34
mintcitrus718-lvtartmui <mintcitrus718@gmail.com>
2025-09-15 16:28:43 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 using namespace std;
 
-
+int element;
+int level;
+int extra;
+int backpack;
 
 int main() {
     cout << "请输入神瞳种类" << endl;
-    
+    cout << "风神瞳[1]/n";
+    cout << "岩神瞳[2]/n";
+    cout << "雷神瞳[3]/n";
+    cout << "草神瞳[4]/n";
+    cout << "水神瞳[5]/n";
+    cout << "火神瞳[6]/n";
+    cout << "月神瞳[7]/n";
+    cin >> element;
+
+    cout << "请输入神像等级";
+    cin >> level;
+
+    cout << "请输入神瞳溢出数量";
+    cin >> extra;
+
+    cout << "请输入背包内神瞳数量";
+    cin >> backpack;
+
+    switch element
 }
