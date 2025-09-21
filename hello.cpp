201084844e11dc58995bbda3f32c99e92f5c7670
mintcitrus718-lvtartmui <mintcitrus718@gmail.com>
2025-09-21 16:16:19 +0800

    oculus-counter
    1.0 支持大世界剩余神瞳计算

--- hello.cpp
+++ hello.cpp
@@ -2,23 +2,22 @@
 
 using namespace std;
 
-int element;
+int element;	//
 int level;
 int extra;
 int backpack;
-int So7;
+int So7; 
 int remain;
 
 int main(){
 
-  int anemo[9]= {1,2,4,6,7,8,10,12,15};
-  int geo[9] = {2,4,8,12,14,16,20,24,30};
-  int electro[9] ={10,12,15,18,20,22,25,28,30};
-  int dendro[9] = {20,22,25,28,30,32,35,38,40};
-  int hydro[9] = {20,22,25,28,30,32,35,38,40};
-  int pyro[9] = {20,22,25,28,30,32,35,38,40};
-  int newmoon[5] = {20,22,25,28,30};            //挪德地图不完整，仅支持5级，游戏版更后更新
-  
+  int anemo[10]= {0,1,2,4,6,7,8,10,12,15};
+  int geo[10] = {0,2,4,8,12,14,16,20,24,30};
+  int electro[10] ={0,10,12,15,18,20,22,25,28,30};
+  int dendro[10] = {0,20,22,25,28,30,32,35,38,40};
+  int hydro[10] = {0,20,22,25,28,30,32,35,38,40};
+  int pyro[10] = {0,20,22,25,28,30,32,35,38,40};
+  int newmoon[6] = {0,20,22,25,28,30};            //挪德地图不完整，仅支持5级，游戏版更后更新
   cout <<"请输入神瞳种类"<< endl;
   cout <<"风神瞳［1］\n";
   cout <<"岩神瞳［2］\n";
@@ -36,57 +35,59 @@ int main(){
   cout <<"请输入背包内神瞳数量\n";
   cin >> backpack;
   
+  
   switch (element){
-    case 1:
-      for (int i=0;i<level;i++){
-        So7 += anemo[i];
-        remain = 66-So7-extra-backpack;
-        cout << remain;
-      }
-      case 2:
-        for(int i=0;i<level;i++){
-          So7 += geo[i];
-          remain = 131-So7-extra-backpack;
-          cout << remain;
-      }
-      case 3:
-        for(int i=0;i<level;i++){
-          So7 += electro[i];
-          remain = 181-So7-extra-backpack;
-          cout << remain;
-      }
-      case 4:
-        for(int i=0;i<level;i++){
-          So7 += dendro[i];
-          remain = 271-So7-extra-backpack;
-          cout << remain;
-      }
-      case 5:
-        for(int i=0;i<level;i++){
-          So7 += hydro[i];
-          remain = 271-So7-extra-backpack;
-          cout << remain;
-      }
-      case 6:
-        for(int i=0;i<level;i++){
-          So7 += pyro[i];
-          remain = 271-So7-extra-backpack;
-          cout << remain;
-      }
-      case 7:
-        for(int i=0;i<level;i++){
-          So7 += newmoon[i];
-          remain = 125-So7-extra-backpack;    //临时
-          cout << remain;
-      }
-    }
-}
-      }
-      case 7:
-        for(int i=0;i<level;i++){
-          So7 += newmoon[i];
-          remain = 125-So7-extra-backpack;    //临时
-          cout << remain;
-      }
-    }
+    	case 1:
+	    	for (int i=0;i<level;i++){
+	        	So7 += anemo[i];
+			}
+			remain = 66-So7-extra-backpack;
+	    	cout << remain;
+			break;
+	    	
+		case 2:
+			for(int i=0;i<level;i++){
+			  So7 += geo[i];
+			
+			}
+			remain = 131-So7-extra-backpack;
+			cout << remain << endl;
+			break;
+		case 3:
+			for(int i=0;i<level;i++){
+			  So7 += electro[i];
+			  
+			}
+	    	remain = 181-So7-extra-backpack;
+			cout << remain << endl;
+		  break;
+		case 4:
+	        for(int i=0;i<level;i++){
+	          So7 += dendro[i];
+	        }
+			remain = 271-So7-extra-backpack;
+	        cout << remain << endl;
+	        break;
+    	case 5:
+	        for(int i=0;i<level;i++){
+		      So7 += hydro[i];   
+	        }	
+	      	remain = 271-So7-extra-backpack;
+		    cout << remain << endl;;
+    	break;
+		case 6:
+	        for(int i=0;i<level;i++){
+	        	So7 += pyro[i];  
+	        }
+			remain = 271-So7-extra-backpack;
+	        cout << remain << endl;;
+    	break;
+		case 7:
+	        for(int i=0;i<level;i++){
+	          So7 += newmoon[i];
+	        } 
+			remain = 125-So7-extra-backpack;    //临时
+	        cout << remain << endl;
+	        break;
+    }	
 }
